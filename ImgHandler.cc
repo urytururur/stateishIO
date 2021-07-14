cab66c0ce0d581cd6fd31766387dee486a9e2df3
Torbjorn <torbjorn.ekstedt@hotmail.com>
2021-07-14 10:43:00 +0000

    16

--- ImgHandler.cc
+++ ImgHandler.cc
@@ -1,16 +1,20 @@
 #include "ImgHandler.h"
+#include <iostream>
+
+using namespace std; 
 
 ImgHandler::ImgHandler():
     cachedTextures{} {}
 
-sf::Texture& ImgHandlerprovideTexture(std::string const& pathToImg)
+sf::Texture& ImgHandler::provideTexture(std::string const& pathToImg)
 {
     if(cachedTextures.find(pathToImg) == cachedTextures.end())
     {
         cachedTextures.insert({pathToImg, sf::Texture{}});
         if(!cachedTextures.find(pathToImg)->second.loadFromFile(pathToImg))
         {
-            return EXIT_FAILURE;
+            	//return EXIT_FAILURE;
+		cout << "failed to read texture." << endl; 
         }
     }
     return cachedTextures.find(pathToImg)->second;
